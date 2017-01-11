48527742e4ff2f71078adcfbdc05f541290bae50
Ivan Vodopiviz <ivanvodo@gmail.com>
2017-01-11 12:34:56 -0300

    Implemented basic keyboard input.

--- src/main.cpp
+++ src/main.cpp
@@ -53,9 +53,61 @@ int main(int argc, char* argv[])
 
 		while (SDL_PollEvent(&e) != 0) 
 		{
-			if( e.type == SDL_QUIT )
+			if(e.type == SDL_QUIT)
 			{ 
 				quit = true;
+			}
+
+			if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+					case SDLK_1: chip8.key[0x1] = 1; break;
+					case SDLK_2: chip8.key[0x2] = 1; break;
+					case SDLK_3: chip8.key[0x3] = 1; break;
+					case SDLK_4: chip8.key[0xC] = 1; break;
+					
+					case SDLK_q: chip8.key[0x4] = 1; break;
+					case SDLK_w: chip8.key[0x5] = 1; break;
+					case SDLK_e: chip8.key[0x6] = 1; break;
+					case SDLK_r: chip8.key[0xD] = 1; break;
+
+					case SDLK_a: chip8.key[0x7] = 1; break;
+					case SDLK_s: chip8.key[0x8] = 1; break;
+					case SDLK_d: chip8.key[0x9] = 1; break;
+					case SDLK_f: chip8.key[0xE] = 1; break;
+
+					case SDLK_z: chip8.key[0xA] = 1; break;
+					case SDLK_x: chip8.key[0x0] = 1; break;
+					case SDLK_c: chip8.key[0xB] = 1; break;
+					case SDLK_v: chip8.key[0xF] = 1; break;
+				}
+			}
+
+			if (e.type == SDL_KEYUP)
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_1: chip8.key[0x1] = 0; break;
+				case SDLK_2: chip8.key[0x2] = 0; break;
+				case SDLK_3: chip8.key[0x3] = 0; break;
+				case SDLK_4: chip8.key[0xC] = 0; break;
+
+				case SDLK_q: chip8.key[0x4] = 0; break;
+				case SDLK_w: chip8.key[0x5] = 0; break;
+				case SDLK_e: chip8.key[0x6] = 0; break;
+				case SDLK_r: chip8.key[0xD] = 0; break;
+
+				case SDLK_a: chip8.key[0x7] = 0; break;
+				case SDLK_s: chip8.key[0x8] = 0; break;
+				case SDLK_d: chip8.key[0x9] = 0; break;
+				case SDLK_f: chip8.key[0xE] = 0; break;
+
+				case SDLK_z: chip8.key[0xA] = 0; break;
+				case SDLK_x: chip8.key[0x0] = 0; break;
+				case SDLK_c: chip8.key[0xB] = 0; break;
+				case SDLK_v: chip8.key[0xF] = 0; break;
+				}
 			}
 		}
 
